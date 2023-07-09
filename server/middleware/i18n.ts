f294d9b136db5dc61af27b270bbd4cd39d1c9d60
AriesAlex <famliamarchenko@yandex.ru>
2023-07-09 17:36:20 +0300

    get rid of spare async

--- server/middleware/i18n.ts
+++ server/middleware/i18n.ts
@@ -10,7 +10,7 @@ for (const locale of locales) {
   i18n.setLocaleMessage(locale.code, fs.readJSONSync('locales/' + locale.file))
 }
 
-export default defineEventHandler(async e => {
+export default defineEventHandler(e => {
   e.context.$t = (key: string) =>
     i18n.t(key, getCookie(e, 'lang') || i18n.fallbackLocale.toString())
 })
